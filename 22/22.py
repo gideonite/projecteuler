cdd1c7f16f9aad4584eee385e3a35e3617710299
Gideon Dresdner <gideond@gmail.com>
2015-03-20 14:33:58 +0200

    problem 22

--- 22/22.py
+++ 22/22.py
@@ -2,4 +2,16 @@ words = []
 with open("p022_names.txt") as f:
   words = list(map(lambda s: s[1:], f.readlines()[0].split("\",")))
 
-words
+# there is an extra " hanging out there...
+words[-1] = words[-1][:-1]
+
+alphavalue = lambda c: ord(c)-ascii_offset+1
+scores = []
+ascii_offset = ord('A')
+for i, name in enumerate(sorted(words)):
+  alphavalues = map(alphavalue, name)
+  alphavalues = list(alphavalues)
+  score = (i+1) * sum(alphavalues)
+  scores.append(score)
+
+sum(scores)
